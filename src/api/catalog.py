bc801fa65f8676fdd8075e0e3e819d8a513dd413
Dionne Gregorio <gregoriodionne@yahoo.com>
2024-09-30 17:46:57 -0700

    update catalog

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -12,7 +12,7 @@ def get_catalog():
     Each unique item combination must have only a single price.
     """
     with db.engine.begin() as connection:
-        green_potion = connection.execute(sqlalchemy.text("SELECT * FROM global_inventory WHERE num_green_potions"))
+        green_potion = connection.execute(sqlalchemy.text("SELECT num_green_potions FROM global_inventory")).scalar()
 
     if green_potion <= 0:
         return []
@@ -21,7 +21,7 @@ def get_catalog():
             {
                 "sku": "GREEN_POTION_0",
                 "name": "green potion",
-                "quantity": 1,
+                "quantity": green_potion,
                 "price": 50,
                 "potion_type": [0, 100, 0, 0],
             }
