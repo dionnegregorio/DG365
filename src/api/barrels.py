030879012997bcf95ecb3a58d58765e2257f7dca
Dionne Gregorio <gregoriodionne@yahoo.com>
2024-10-02 13:55:40 -0700

    barrel plan update

--- src/api/barrels.py
+++ src/api/barrels.py
@@ -24,8 +24,7 @@ def post_deliver_barrels(barrels_delivered: list[Barrel], order_id: int):
     """ """
     print(f"barrels delievered: {barrels_delivered} order_id: {order_id}")
 
-    barrels_amount = 0
-
+  
     #save first index of the barrel list for now
     barrels_amount = barrels_delivered[0].ml_per_barrel * barrels_delivered[0].quantity
     gold_spent = barrels_delivered[0].price * barrels_delivered[0].quantity
@@ -43,20 +42,27 @@ def post_deliver_barrels(barrels_delivered: list[Barrel], order_id: int):
 def get_wholesale_purchase_plan(wholesale_catalog: list[Barrel]):
 
     """purchase a new small green potion barrel only if the potions 
-    in inventory is less than 10.Always mix all available green ml 
+    in inventory is less than 10. Always mix all available green ml 
      if any exists. Offer up for sale in the catalog only the amount 
      of green potions that actually exist currently in inventory."""
-    
 
     with db.engine.begin() as connection:
-        green_potion = connection.execute(sqlalchemy.text("SELECT num_green_potions FROM global_inventory")).scalar
+        num_green_potion = connection.execute(sqlalchemy.text("SELECT num_green_potions FROM global_inventory")).scalar
     
         gold_total = connection.execute(sqlalchemy.text("SELECT gold FROM global_inventory")).scalar()
 
+    inventory = {
+        "Green_ml" : num_green_potion
+        #"Red_ml" : 
+       #"Blue_ml" : 
+
+    }
+   
     plan = []
+
     for barrel in wholesale_catalog:
 
-        if barrel.sku == "SMALL_GREEN_BARREL" and green_potion < 10:
+        if barrel.sku == "SMALL_GREEN_BARREL" and inventory.get("Green_ml") < 10:
             can_buy = gold_total // barrel.price
             if can_buy <= 0:
                 continue
