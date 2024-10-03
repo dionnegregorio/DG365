eeb63a06a4b9a84fb87f70af41e5039655745755
Dionne Gregorio <gregoriodionne@yahoo.com>
2024-10-02 22:05:10 -0700

    barrel plan debugging

--- src/api/barrels.py
+++ src/api/barrels.py
@@ -24,10 +24,9 @@ def post_deliver_barrels(barrels_delivered: list[Barrel], order_id: int):
     """ """
     print(f"barrels delievered: {barrels_delivered} order_id: {order_id}")
 
-  
-    #save first index of the barrel list for now
-    barrels_amount = barrels_delivered[0].ml_per_barrel * barrels_delivered[0].quantity
-    gold_spent = barrels_delivered[0].price * barrels_delivered[0].quantity
+    for barrel in barrels_delivered:
+        barrels_amount = barrels_delivered[barrel].ml_per_barrel * barrels_delivered[barrel].quantity
+        gold_spent = barrels_delivered[barrel].price * barrels_delivered[barrel].quantity
   
 
     with db.engine.begin() as connection:
@@ -47,30 +46,54 @@ def get_wholesale_purchase_plan(wholesale_catalog: list[Barrel]):
      of green potions that actually exist currently in inventory."""
     
     print(wholesale_catalog)
-
     with db.engine.begin() as connection:
-        num_green_potion = connection.execute(sqlalchemy.text("SELECT num_green_potions FROM global_inventory"))
+        current_green_potions = connection.execute(sqlalchemy.text("SELECT num_green_potions FROM global_inventory")).scalar()
+        gold_total = connection.execute(sqlalchemy.text("SELECT num_green_potions FROM global_inventory")).scalar()
+
+    if current_green_potions < 10 and gold_total >= cost:
+            return [
+                {
+                "sku": "SMALL_GREEN_BARREL",
+                "quantity": 5,
+                }
+            ]
+    
+    print(wholesale_catalog)
+
+    return []
+
+    """with db.engine.begin() as connection:
+        num_green_potion = connection.execute(sqlalchemy.text("SELECT num_green_potions FROM global_inventory")).scalar()
         gold = connection.execute(sqlalchemy.text("SELECT gold FROM global_inventory"))  
         num_green_potion = num_green_potion.first()
         gold = gold.first()
 
-    """inventory = {  
+    inventory = {  
         "Green_ml" : num_green_potion
         #"Red_ml" : num_red_potion
        #"Blue_ml" : num_blue_potion
-    }"""
+    }
 
-    plan = []
+    barrel_plan = []
 
     for barrel in wholesale_catalog:
-        if barrel.sku == "SMALL_GREEN_BARREL" and num_green_potion < 10:
+        if barrel.sku.upper() != "SMALL_GREEN_BARREL": #and num_green_potion < 10:
+            return [
+                {"sku": "SMALL_GREEN_BARREL", "quantity": 5}
+                ]
+        else: 
+            return [
+                {"sku": "SMALL_GREEN_BARREL", "quantity": 5}
+                ]
+            
             amount_can_buy = gold // barrel.price
             if amount_can_buy <= 0:
                 continue
             if amount_can_buy >= barrel.quantity:
                 amount_can_buy = barrel.quantity
-                plan.append({"sku": barrel.sku, "quantity": amount_can_buy})
+                barrel_plan.append({"sku": barrel.sku, "quantity": amount_can_buy})
             else: 
-                plan.append({"sku": barrel.sku, "quantity": amount_can_buy})
-    return plan
+                barrel_plan.append({"sku": barrel.sku, "quantity": amount_can_buy})
+    #return barrel_plan
+    """
         
