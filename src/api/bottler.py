cc75e240b048a27fe3af37428f46c603d9006926
Dionne Gregorio <gregoriodionne@yahoo.com>
2024-10-03 23:29:44 -0700

    bot. deliv.

--- src/api/bottler.py
+++ src/api/bottler.py
@@ -21,19 +21,19 @@ def post_deliver_bottles(potions_delivered: list[PotionInventory], order_id: int
     print(f"potions delievered: {potions_delivered} order_id: {order_id}")
 
     with db.engine.begin() as connection:
-        #green_ml = connection.execute(sqlalchemy.text("SELECT num_green_ml FROM global_inventory")).scalar()
+        green_ml = connection.execute(sqlalchemy.text("SELECT num_green_ml FROM global_inventory")).scalar()
         green_potions = connection.execute(sqlalchemy.text("SELECT num_green_potions FROM global_inventory")).scalar()
 
+    amount_of_bottles += green_potions
 
-    amount_of_bottles = 0
     for potion in PotionInventory:
         if potion.quantity > 0:
-            amount_of_bottles = potion.quantity + green_potions
-            connection.execute(sqlalchemy.text(f"UPDATE global_inventory SET num_green_potions = {amount_of_bottles})
+            amount_of_bottles = potion.quantity
+            connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_green_potions =:amount_of_bottles"))
+
+
 
     return "OK"
-
-
 
 @router.post("/plan")
 def get_bottle_plan():
@@ -55,10 +55,9 @@ def get_bottle_plan():
         return [
             {
                 "potion_type": [0, 100, 0, 0],
-                "quantity": result,
+                "quantity": 5,
             }
         ]
 
 if __name__ == "__main__":
     print(get_bottle_plan())
- 
